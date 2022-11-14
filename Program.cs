5c5e5875787855bbebe4e009f5fb7f7523a3d277
Arthur Mattos <arsmmd@hotmail.com>
2022-11-14 09:51:47 -0300

    Correções para executar no .NET 7

--- Program.cs
+++ Program.cs
@@ -1,27 +1,58 @@
-using System.IO;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using CustomerOrdersApi.Config;
+using MongoDB.Bson.Serialization.Conventions;
 
 namespace CustomerOrdersApi
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-           var config = new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
-                .Build();
+      var builder = WebApplication.CreateBuilder(args);
 
-            var host = new WebHostBuilder()
-                .UseConfiguration(config)
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+      var env = builder.Environment;
 
-            host.Run();
-        }
+      builder.Configuration
+        .SetBasePath(env.ContentRootPath)
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+        .AddEnvironmentVariables();
+
+      var pack = new ConventionPack();
+      pack.Add(new CamelCaseElementNameConvention());
+      ConventionRegistry.Register("camel case", pack, t => true);
+
+      // Add services to the container.
+
+      builder.Services
+        .Configure<AppSettings>(builder.Configuration)
+        .AddControllers()
+                  /*
+                            .AddMvcOptions(c =>
+                            {
+                              c.EnableEndpointRouting = false;
+                              c.OutputFormatters.Add(new JsonHalOutputFormatter(
+                                  new Newtonsoft.Json.JsonSerializerSettings(),
+                                  halJsonMediaTypes: new string[] { "application/hal+json", "application/vnd.example.hal+json", "application/vnd.example.hal.v1+json" }
+                              ));
+                            })*/
+                  ;
+
+      var app = builder.Build();
+
+      // Configure the HTTP request pipeline.
+
+      //app.UseHttpsRedirection();
+
+      //app.UseAuthorization();
+
+      app.MapControllers();
+
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.Run();
     }
+  }
 }
